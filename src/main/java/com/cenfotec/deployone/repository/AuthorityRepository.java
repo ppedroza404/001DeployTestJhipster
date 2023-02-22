8ede0ace628fdca077c5bfa919e96ee33c52291e
Pablo <ppedrozas@ucenfotec.ac.cr>
2023-02-21 19:30:11 -0600

    Initial version of 001Deploy generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/cenfotec/deployone/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.cenfotec.deployone.repository;
+
+import com.cenfotec.deployone.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
